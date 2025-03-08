aa69417ab9edf83e118b55f6f551015268fe65e9
Wanderson Brito <wanderson.brito.wbda@gmail.com>
2025-03-08 04:26:33 -0300

    Update2

--- components/ui/layout/footer.tsx
+++ components/ui/layout/footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="mb-4 md:mb-0">
             <p>
-              © 2024 Clã TeenWolf. Todos os direitos reservados.
+              © 2024 Clã TeenWolf. Todos os direitos reservados para o jogo Smash Legends.
             </p>
           </div>
           <div className="flex flex-col items-end">
